162ca6a9976435f67693ba50db4d506953ba85b6
MadTown86 <101869180+MadTown86@users.noreply.github.com>
2022-12-03 14:20:28 -0600

    Correct Answer(s) for day 3
    These are fun.

--- dec3rd.py
+++ dec3rd.py
@@ -5,12 +5,15 @@ dv = dotenv.DotEnv(os.getenv('localAdvent2022'))
 lp_proj = dv.get("root_local")
 fname = lp_proj + "\\" + 'dec3rd_input.txt'
 
+"""
+Learning recap - I didn't need to do all of the garbage trying to 'slice' off the newline characters, they aren't being
+considered at least they don't matter for the second part of the 3rd days question.
+"""
 
 def rucksackdecode(fname):
     with open(fname, 'r') as fp:
         tot = 0
         tolistorg = fp.readlines()
-        print(len(tolistorg))
         tolist = [[x[:(len(x[:-1]) // 2)], x[:-1][len(x[:-1]) // 2:]] for x in tolistorg if x != tolistorg[-1]]
         tolist.append([tolistorg[-1][:(len(tolistorg[-1]) // 2)], tolistorg[-1][len(tolistorg[-1]) // 2:]])
         for x, y in tolist:
@@ -31,12 +34,10 @@ def badgeofhonor(fname):
     with open(fname, 'r') as fp:
         tolistorg = fp.readlines()
         tlo = [x for x in tolistorg]
-        print(tlo)
         i = 0
         j = 1
         k = 2
         tot = 0
-        testbin = {}
         while k < len(tlo):
             flag = False
             for dig in tlo[i]:
@@ -49,19 +50,11 @@ def badgeofhonor(fname):
                         else:
                             calc = (ord(dig) - ord('a')) + 1
                             tot += calc
-
-                        testbin[i] = (dig, calc)
-
             i += 3
             j += 3
             k += 3
 
-        return tot, testbin
-
-
-
-
-
+        return tot
 
 if __name__ == "__main__":
     print(badgeofhonor(fname))
