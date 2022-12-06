4ab4c9486e14155d65f038842d4eb3d835c8e848
MadTown86 <101869180+MadTown86@users.noreply.github.com>
2022-12-05 20:46:59 -0600

    Correct Answer(s) for day 4
    These are fun.

--- dec4th.py
+++ dec4th.py
@@ -13,11 +13,27 @@ def innyoutty(fname):
             f, s = x.split('-')
             t, fo = y.split('-')
             terms = lambda f, s, t, fo: [f-t, s-fo]
+            a, b = terms(int(f), int(s), int(t), int(fo))
+            conditions = True if a <= 0 <= b or a >= 0 >= b else False
+            if conditions:
+                count += 1
+        return count
 
-            if terms:
+def overlappy(fname):
+    with open(fname, 'r') as fp:
+        count = 0
+        inp = [x[:-1].split(',') for x in fp.readlines()]
+        for x, y in inp:
+            f, s = x.split('-')
+            t, fo = y.split('-')
+            cond1 = True if int(f) < int(t) > int(s) or int(f) > int(fo) < int(s) else False
+
+            if cond1:
+                continue
+            else:
                 count += 1
 
         return count
 
 if __name__ == "__main__":
-    print(innyoutty(fname))
+    print(overlappy(fname))
