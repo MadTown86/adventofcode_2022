1fdca5fcd866ea90611a702aa1276f03b2b5f270
MadTown86 <101869180+MadTown86@users.noreply.github.com>
2022-12-07 03:53:23 -0600

    Day 6 Down

--- dec6th.py
+++ dec6th.py
@@ -9,25 +9,32 @@ def signal(fname):
     with open(fname) as fp:
         i = fp.read()
         j = 3
-        test = True if i[j] != i[j - 1] and i[j] != i[j - 2] and i[j] != i[j - 3] and i[j - 2] != i[j - 3] else False
-        t1 = True if i[j] != i[j-1] else False
-        t2 = True if i[j] != i[j-2] else False
-        t3 = True if i[j] != i[j-3])
-        print(i[j-2] != i[j-3])
-
-
         top = len(i)
         while j < top:
-            print(i[j] != i[j - 1])
-            print(i[j] != i[j - 2])
-            print(i[j] != i[j - 3])
-            print(i[j - 2] != i[j - 3])
-            print(test, j, i[j-3], i[j-2], i[j-1], i[j])
-            if test:
-                return j
+            t1 = True if i[j] != i[j-1] else False
+            t2 = True if i[j] != i[j-2] else False
+            t3 = True if i[j] != i[j-3] else False
+            t4 = True if i[j-2] != i[j-3] else False
+            t5 = True if i[j-1] != i[j-2] else False
+            t6 = True if i[j-1] != i[j-3] else False
+            if t1 and t2 and t3 and t4 and t5 and t6:
+                return j + 1, i[j], i[j-1], i[j-2], i[j-3]
             else:
                 j += 1
 
+def signal2(fname):
+    with open(fname) as fp:
+        i, p, b = fp.read(), 0, []
+        while len(b) < 14:
+            if i[p] not in b:
+                b.append(i[p])
+                p += 1
+            else:
+                fallback = len(b) - b.index(i[p]) - 1
+                b = []
+                p -= fallback
+        return p
+
 if __name__ == "__main__":
-    print(signal(fname))
+    print(signal2(fname))
 
