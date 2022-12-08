2b4f03830aea60107d24a5eed5796c0e52c76e4b
MadTown86 <101869180+MadTown86@users.noreply.github.com>
2022-12-08 15:28:19 -0600

    Start 6

--- dec7th.py
+++ dec7th.py
@@ -7,13 +7,13 @@ fname = lp_proj + "\\" + 'dec7thinput.txt'
 
 class TreeMake:
     class Node:
-        __slots__ = '_parent', '_element', '_children', '_siblings', 'num_children'
+        __slots__ = '_parent', '_element', '_children', '_sibling', 'num_children'
 
         def __init__(self, parent, element, sibling, child):
             self._parent = parent
             self._element = element
             self._children = child
-            self._siblings = sibling
+            self._sibling = sibling
             self.num_children = 0
 
     class Position:
@@ -24,8 +24,11 @@ class TreeMake:
         def _element(self):
             return self._node._element
 
+        def _sibling(self):
+            return self._node._sibling is True
+
         def __eq__(self, other):
-            return True if type(self) is type(other) and self._node is other._node else False
+            return type(self) is type(other) and self._node is other._node
 
         def __ne__(self, other):
             return self == other
@@ -35,8 +38,6 @@ class TreeMake:
             raise TypeError('Not a valid posiition')
         if p._container is not self:
             raise ValueError('Does not belong to this contaienr')
-        if p._node._children is None:
-            raise ValueError('Position is no longer valid')
         return p._node
 
     def _make_position(self, node):
@@ -49,8 +50,13 @@ class TreeMake:
     def _is_empty(self):
         return self._size == 0
 
-    def _rootf(self):
-        self._root = self.Node(None, None, None)
+    def p_next(self, p):
+        org = self._validate(p)
+        if org
+        return p._sibling
+
+    def rootf(self):
+        self._root = self.Node(None, None, None, None)
         self._size += 1
         return self._make_position(self._root)
 
@@ -63,17 +69,20 @@ class TreeMake:
         return self._make_position(self._root)
 
     def get_element(self, p):
-        return p._element
-    def add_child(self, parent: Node, e):
-        parent._children = self.Node(parent, e, None, None)
-        parent.num_children += 1
-        self._size += 1
-        return self._make_position(parent._children)
+        org = self._validate(p)
+        return org._element
+    def add_child(self, e, p: Position):
+        original = self._validate(p)
+        newnode = self.Node(original, e, None, None)
+        original.num_children += 1
+        original._children = newnode
+        return self._make_position(newnode)
 
-    def add_sibling(self, sibling: Node, e):
-        sibling._siblings = self.Node(sibling._parent, e, None, None)
-        self._size += 1
-        return self._make_position(sibling._siblings)
+    def add_sibling(self, p, e):
+        original = self._validate(p)
+        newest = self.Node(original._parent, e, None, None)
+        original._sibling = newest
+        return self._make_position(newest)
 
     def first_sibling(self, p):
         node = self._validate(p)
@@ -84,10 +93,10 @@ class TreeMake:
         return node.num_children != 0
 
     def after(self, p):
-        if self.is_leaf(p):
-            return None
         node = self._validate(p)
-        return self._make_position(node._siblings)
+        if node._sibling is None:
+            return None
+        return self._make_position(node._sibling)
 
     def down(self, p):
         if self.is_leaf(p):
@@ -101,19 +110,18 @@ class TreeMake:
         else:
             if self.is_leaf(p):
                 return None
-            while self.
-
-
-
+            while self.after(p) is not None:
+                p = self.after(p)
+                self.traverse(p, e)
+            p = self.first_sibling(p)
+            while self.after(p) is not None:
+                p = self.down(p)
+                self.traverse(p, e)
 
     def change_directory(self, e):
         if not self.is_root():
             raise Exception('No Root')
         walk = self.at_root()
-
-
-
-
 
     def is_leaf(self, p):
         node = self._validate(p)
@@ -121,12 +129,32 @@ class TreeMake:
 
     def build(self, fname):
         commands = {
-            '$ cd /': lambda: self._rootf()
-            '$ ls ':
+            '$ cd /': lambda: self._rootf(),
+            '$ ls ': None
         }
         with open(fname, 'r') as fp:
             i = fp.read()
             i_shed = []
+
+
+
+if __name__ == "__main__":
+    T = TreeMake()
+    N1 = T.rootf()
+    N2 = T.add_child("BB1", N1)
+    N3 = T.add_sibling(N2, "BB2")
+    N4 = T.add_sibling(N3, "BB3")
+    N5 = T.add_sibling(N4, "BB4")
+    # N6 = T.first_sibling(N4)
+    # N6 = T.add_child("FORALL", N5)
+    p = N2
+    while True:
+        if T.p_next(p):
+            print(p._element)
+            p = T.after(p)
+
+
+
 
 
 
