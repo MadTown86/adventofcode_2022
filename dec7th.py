88b622873af5ce7f2a14979975c5e50e42ce02e0
MadTown86 <101869180+MadTown86@users.noreply.github.com>
2022-12-28 15:14:52 -0600

    Tree Structure - Work Update

--- dec7th.py
+++ dec7th.py
@@ -1,7 +1,7 @@
 import dotenv
 import os
 from collections import deque
-lp_proj = dotenv.get_key(os.getenv('adventofcode2022'), 'root_local')
+lp_proj = dotenv.get_key(os.getenv('localAdvent2022'), 'root_local')
 fname = lp_proj + "\\" + 'dec7thinput.txt'
 
 
@@ -107,24 +107,24 @@ class TreeMake:
         if node._first_child is not None:
             raise ValueError("First child already exists")
         node._first_child = self.Node(e, parent=node)
-        node._first_child.num_siblings += 1
         node.num_children += 1
         return self._make_position(node._first_child)
 
     def _add_right_sibling(self, p, e):
         # Add sibling to right of given sibling, if no first sibling, create
         node = self._validate(p)
-        if node._right_sibling is not None:
-            while node._right_sibling is not None:
-                node = node._right_sibling
-            node._right_sibling = self.Node(e, parent=node._parent)
-            node._parent.num_children += 1
-            node._parent._first_child.num_siblings += 1
-            return self._make_position(node._right_sibling)
-        node._right_sibling = self.Node(e, parent=node._parent)
-        node._parent.num_children += 1
-        node._parent._first_child.num_siblings += 1
-        return self._make_position(node._right_sibling)
+        if node._first_child == None:
+            new_node = self.Node(e, parent=node)
+            node._first_child = new_node
+            node.num_children += 1
+            return self._make_position(new_node)
+        cur = node._first_child
+        while cur._right_sibling is not None:
+            cur = cur._right_sibling
+        new_node = self.Node(e, parent=node)
+        cur._right_sibling = new_node
+        node.num_children += 1
+        return self._make_position(new_node)
 
     def _replace_value(self, p, e):
         # Replace value at p, return old value
@@ -170,90 +170,90 @@ class TreeMake:
     def _breadth_bin(self, p):
         res = []
         node = self._validate(p)
-        while node._right_sibling is not None:
+        if node._first_child is None:
+            return []
+        if node is self._root:
+            node = self._root._first_child
+            while node._right_sibling is not None:
+                res.append(node)
+                node = node._right_sibling
             res.append(node)
-            node = node._right_sibling
-        res.append(self._make_position(node))
+            return res
+        pos = node._first_child
+        while pos._right_sibling is not None:
+            res.append(pos._element)
+        res.append(pos._element)
         return res
 
     def _file_search(self, v):
         q = deque()
-        start = self._atroot()
-        r_node = self._validate(start)
-        if r_node._element == v:
-            return start
-        if not self._num_children(start) > 0:
+        if not self._root:
+            raise TypeError("Invalid Tree")
+        if v == self._root._element:
+            return self._make_position(self._root)
+        if self._num_children(self._make_position(self._root)) == 0:
             raise ValueError("Empty Tree")
-        node = self._validate(start)
+        print(self._root._element)
+        print(self._root._first_child._element)
+        q.append(self._root._first_child)
+        pos = self._root._first_child
+        while pos._right_sibling is not None:
+            q.append(pos._right_sibling)
+            pos = pos._right_sibling
+        q.append(pos)
 
-        def diver(node, v):
-            print(f'DIVER STEP: {node._element}')
-            if node._element == v:
-                return self._make_position(node)
-            while node._first_child is not None:
-                print(f'WHILE DIVER STEP: {node._element}')
-                if node._element == v:
-                    return self._make_position(node)
-                if node.num_siblings == 0:
-                    node = node._first_child
-                    continue
-                q.append(node._right_sibling)
-                node = node._first_child
-            if node._element == v:
-                return self._make_position(node)
-            if node._right_sibling:
-                q.append(node._right_sibling)
-
-
-        diver(node, v)
-
-        # Finish loop through deck to clear it of everything that doesn't either populate it with more values
-        # Is the value looking for
-        # Or the deque is empty
         while len(q) > 0:
-            node = q.popleft()
-            if node == None:
-                raise ValueError("Value Not Found") and StopIteration("End Of Tree")
-            print(f'QUEUE STEP: {node._element}')
-            p = self._make_position(node)
-            if node._element == v:
-                return self._make_position(node)
-            if self._is_leaf(p):
-                if self._num_siblings(p) is not None:
-                    q.append(node._right_sibling)
-            else:
-                diver(node, v)
+            pos = q.popleft()
+            if pos._element == v:
+                return self._make_position(pos)
+            if pos.num_children > 0:
+                q.append(pos._first_child)
+                pos = pos._first_child
+                while pos._right_sibling is not None:
+                    q.append(pos._right_sibling)
+                    pos = pos._right_sibling
+                q.append(pos)
 
-    def first_child(self, p):
-        node = self._validate(p)
-        if node._first_child:
-            return node._first_child._element
-        else:
-            raise ValueError("No Children")
+        return "Not Found"
 
     def treemake(self, fname):
         with open(fname, 'r') as fp:
             line1 = fp.readline()
-            root = self._rootbegin(line1[5:])
+            self._rootbegin(line1[5:-1])
             fp.seek(0)
             for line in fp.readlines():
                 print(line)
                 if line[:1] == '$':
-                    if 'cd' in line[:1]:
-                        curr_p = self._file_search(line[5:])
-                    if 'ls' in line[:1]:
+                    if 'cd' in line:
+                        if '..' in line:
+                            continue
+                        else:
+                            currval = line[5:-1]
+                            curr_p = self._file_search(currval)
+                            continue
+                    if 'ls' in line:
                         continue
                 if 'dir' in line:
-
+                    print(self._validate(curr_p)._element)
                     check_bin = self._breadth_bin(curr_p)
                     if line[5:-1] in check_bin:
                         continue
                     else:
                         node = self._validate(curr_p)
+                        addval = line[4:-1]
                         if node.num_children == 0:
-                            self._add_first_child(curr_p, line[5:-1])
-                        add_pos = self._at_last_sibling(self._atfirst_child(curr_p))
-                        self._add_right_sibling(add_pos, line[5:-1])
+                            self._add_first_child(curr_p, addval)
+                            continue
+                        add_pos = self._atfirst_child(curr_p)
+                        self._add_right_sibling(add_pos, addval)
+                        continue
+                elif 'cd' and 'dir' not in line:
+                    node = self._validate(curr_p)
+                    addval = line.split(' ')
+                    if node.num_children == 0:
+                        self._add_first_child(curr_p, addval)
+                    else:
+                        self._add_right_sibling(curr_p, addval)
 
 
 if __name__ == "__main__":
@@ -261,14 +261,30 @@ if __name__ == "__main__":
     T.treemake(fname)
 
 
-    NRoot = T._rootbegin("Root")
-    NA1 = T._add_first_child(p=NRoot, e="A1")
-    NA2 = T._add_right_sibling(NA1, "A2")
-    NA3 = T._add_right_sibling(NA2, "A3")
-    NB1 = T._add_first_child(T._atfirst_child(NRoot), "B1")
-    NB2 = T._add_right_sibling(NB1, "B2")
-    NB3 = T._add_right_sibling(NB2, "B3")
-    AB1 = T._add_right_sibling(T._at_parent(NB3), "AB1")
+    # NRoot = T._rootbegin("Root")
+    # NA1 = T._add_first_child(p=NRoot, e="1a")
+    # NA2 = T._add_right_sibling(NA1, "2a")
+    # NA3 = T._add_right_sibling(NA2, "3a")
+    # NB1 = T._add_first_child(T._atfirst_child(NRoot), "1ab")
+    # NB2 = T._add_right_sibling(NB1, "2ab")
+    # NB3 = T._add_right_sibling(NB2, "3ab")
+    # NB3ab1 = T._add_first_child(NB3, "3ab1")
+    # NB3ab2 = T._add_right_sibling(NB3ab1, "3ab2")
+    # NB3ab3 = T._add_right_sibling(NB3ab2, "3ab3")
+    # N3a1 = T._add_first_child(NA3, "3a1")
+    # N3a2 = T._add_right_sibling(N3a1, "3a2")
+    # N3a2a = T._add_first_child(N3a2, "3a2a")
+    # N3a2b = T._add_right_sibling(N3a2a, "3a2b")
+    # N3a2a1 = T._add_first_child(N3a2a, "3a2a1")
+    # AB1 = T._add_right_sibling(T._at_parent(NB3), "AB1")
+    #
+    # def pos_printer(p):
+    #     node = T._validate(p)
+    #     print(node._element)
+    #
+    # tl = ["2a", "3a", "3ab1", "3ab2", "3a2a1"]
+    # for x in tl:
+    #     pos_printer(T._file_search(x))
 
     # for x in T._breadth(NA1):
     #     print(x._element())
