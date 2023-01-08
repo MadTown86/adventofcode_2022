4826b44485ef4bdccb0855d4a5733c7a950778df
MadTown86 <grover.donlon@gmail.com>
2023-01-08 00:44:31 -0600

    Part 1 Dec 8th

--- dec8th.py
+++ dec8th.py
@@ -1,69 +1,108 @@
 import dotenv, os
 
 lp_proj = dotenv.get_key(os.getenv('adventofcode2022'), 'root_local')
-fname = lp_proj + "\\" + 'dec8thexample.txt'
+fname = lp_proj + "\\" + 'dec8thinput.txt'
 
 with open(fname, 'r') as f:
     lines = f.read().splitlines()
 
-x, y, count = 0, 0, 0
+totl = len(lines)
+count = 0
+res_list = []
 
-print(len(lines))
-print(len(lines[0]))
+step_x = lambda x: lines[x][y]
+step_y = lambda y: lines[x][y]
+nrange = lambda x: list(range(x - 1, -1, -1))
+srange = lambda x: list(range(x + 1, totl, 1))
+erange = lambda y: list(range(y + 1, totl, 1))
+wrange = lambda y: list(range(y - 1, -1, -1))
 
-for x in range(len(lines)):
-    for y in range(len(lines[0])):
-        if x == 0 or x == len(lines)-1:
+for x in range(totl):
+    for y in range(totl):
+
+        if x == 0 or y == 0 or x == totl - 1 or y == totl - 1:
             count += 1
-        if y == 0 or y == len(lines[0])-1:
-            count += 1
+            continue
 
-    if 0 < x < 99 and 0 < y < 99:
         pos = lines[x][y]
-        n = lines[x-1][y]
-        s = lines[x+1][y]
-        e = lines[x][y+1]
-        w = lines[x][y-1]
-        if lines[x-1][y] < pos:
-            for ind in range(x, 0, -1):
-                if lines[ind][y] > pos:
-                    break
-                if ind == 0:
+
+        flag = True
+        for xn in nrange(x):
+            if pos <= step_x(xn):
+                break
+            if xn == 0:
+                if pos > step_x(xn):
                     count += 1
-        # if lines[x+1][y] < pos:
-        #     for ind in range(x, 99, 1):
-        #         if lines[ind][y] > pos:
-        #             break
-        #         if ind == 99:
-        #             count += 1
-        # if lines[x][y+1] < pos:
-        #     for ind in range(y, 99, 1):
-        #         if lines[x][ind] > pos:
-        #             break
-        #         if ind == 99:
-        #             count += 1
-        # if lines[x][y-1] < pos:
-        #     for ind in range(y, 0, -1):
-        #         if lines[x][ind] > pos:
-        #             break
-        #         if ind == 0:
+                    res_list.append(([x, y], pos, count))
+                    flag = False
+                    break
+        if flag:
+            for xs in srange(x):
+                if pos <= step_x(xs):
+                    break
+                if xs == totl-1:
+                    if pos > step_x(xs):
+                        count += 1
+                        res_list.append(([x, y], pos, count))
+                        flag = False
+                        break
+        if flag:
+            for ye in erange(y):
+                if pos <= step_y(ye):
+                    break
+                if ye == totl-1:
+                    if pos > step_y(ye):
+                        count += 1
+                        res_list.append(([x, y], pos, count))
+                        flag = False
+                        break
+        if flag:
+            for yw in wrange(y):
+                if pos <= step_y(yw):
+                    break
+                if yw == 0:
+                    if pos > step_y(yw):
+                        count += 1
+                        res_list.append(([x, y], pos, count))
+                        flag = False
+                        break
 
-print(count)
+print(res_list)
+print(f' ANS1: {count}')
+
+
+res_list2 = []
+ncount, scount, ecount, wcount, rank = 0, 0, 0, 0, 0
+for x in range(totl):
+    for y in range(totl):
+
+        pos = lines[x][y]
+
+
+        for xn in nrange(x):
+            if pos > step_x(xn):
+                ncount += 1
+            if pos <= step_x(xn):
+                break
+        for xs in srange(x):
+            if pos > step_x(xs):
+                scount += 1
+            if pos <= step_x(xs):
+                break
+        for ye in erange(y):
+            if pos > step_y(ye):
+                ecount += 1
+            if pos <= step_y(ye):
+                break
+        for yw in wrange(y):
+            if pos > step_y(yw):
+                wcount += 1
+            if pos <= step_y(yw):
+                break
+
+        rank = ncount * scount *
+        res_list2.append(([x, y], pos, rank))
 
 
 
 
-
-
-
-
-
-
-
-
-
-# loop through each position
-# at each position check its n, s, e, w positions to see shorter/taller
-# continue in each direction that is smaller until you reach an edge or a taller tree
-# maintain count of trees viewable from exterior
-# Count exterior trees
