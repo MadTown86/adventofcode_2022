40e6112da1170f0727a24cbae083283b38df53ea
MadTown86 <101869180+MadTown86@users.noreply.github.com>
2023-01-15 16:30:05 -0600

    Lines vs. Hours

--- dec9th.py
+++ dec9th.py
@@ -7,8 +7,9 @@ fname = os.path.join(input_dir, 'dec9thexample.txt')
 
 tail_positions = []
 xh, xt, yh, ht = 0, 0, 0, 0
-class Snake:
 
+
+class Snake:
     class Node:
         __slots__ = 'x', 'y'
 
@@ -19,28 +20,32 @@ class Snake:
         def _coord(self):
             return (self.x, self.y)
 
-
     def __init__(self):
         self.head = self.Node()
         self.tail = self.Node()
         self.positions = {(self.tail.x, self.tail.y)}
 
-
     def _move_head(self, left: int = None, right: int = None, up: int = None, down: int = None):
+        old = [self.head.x, self.head.y]
+
         if left:
             self.head.x = self.head.x - left
+            return "left"
         if right:
             self.head.x = self.head.x + right
+            return "right"
         if up:
             self.head.y = self.head.y + up
+            return "up"
         if down:
             self.head.y = self.head.y - down
+            return "down"
 
-    def _move_tail(self):
+    def _move_tail(self, direc: 'str' = "up"):
         x_dif = self.head.x - self.tail.x
         y_dif = self.head.y - self.tail.y
 
-        def same_axis(delt, x=True):
+        def same_axis(delt, x=True, ):
             if delt > 0:
                 while delt > 1:
                     if x:
@@ -66,13 +71,42 @@ class Snake:
             same_axis(x_dif, x=False)
 
         # 1 STEP MOVES:
+
         if x_dif == 1 and y_dif == 1:
+            # before h/y | after h/y - h(3, 3)t(3, 2) after: h(4, 3)t(3,2)
+            if direc == "right":
+                self.tail.y += 1
+            # before: h(3, 3) t(2, 3) - after: h(3, 4) t(2, 3)
+            if direc == "up":
+                self.tail.x += 1
+        if x_dif == -1 and y_dif == 1:
+            # before h(3, 3) t(3, 2) after: h(2, 3) t(3, 2)
+            if direc == "left":
+                self.tail.y += 1
+            if direc == "up":
+                self.tail.x -= 1
+        if x_dif == -1 and y_dif == -1:
+            if direc == "left":
+                self.tail.y -= 1
+            if direc == "down":
+                self.tail.x -= 1
+        if x_dif == 1 and y_dif == -1:
+            if direc == "down":
+                self.tail.x += 1
+            if direc == "right":
+                self.tail.y -= 1
 
+        if x_dif < y_dif:
+            
+            pass
 
+        if x_dif > y_dif:
 
+            pass
 
+        if x_dif == 0 and y_dif == 0:
 
-
+            pass
 
     def snake_run(self, f: str):
         with open(f, 'r') as fl:
@@ -80,25 +114,26 @@ class Snake:
         for line in lines:
             print(line)
             if "L" in line:
-                self._move_head(left=int(line[2:].strip()))
+                hm = self._move_head(left=int(line[2:].strip()))
                 print(f'HEAD: {self.head.x, self.head.y}')
-                self._move_tail()
+                self._move_tail(hm)
                 print(f'TAIL: {self.tail.x, self.tail.y}')
             if "R" in line:
-                self._move_head(right=int(line[2:].strip()))
+                hm = self._move_head(right=int(line[2:].strip()))
                 print(f'HEAD: {self.head.x, self.head.y}')
-                self._move_tail()
+                self._move_tail(hm)
                 print(f'TAIL: {self.tail.x, self.tail.y}')
             if "U" in line:
-                self._move_head(up=int(line[2:].strip()))
+                hm = self._move_head(up=int(line[2:].strip()))
                 print(f'HEAD: {self.head.x, self.head.y}')
-                self._move_tail()
+                self._move_tail(hm)
                 print(f'TAIL: {self.tail.x, self.tail.y}')
             if "D" in line:
-                self._move_head(down=int(line[2:].strip()))
+                hm = self._move_head(down=int(line[2:].strip()))
                 print(f'HEAD: {self.head.x, self.head.y}')
-                self._move_tail()
+                self._move_tail(hm)
                 print(f'TAIL: {self.tail.x, self.tail.y}')
+
 
 if __name__ == "__main__":
     S = Snake()
